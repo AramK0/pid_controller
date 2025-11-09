69c690eee97fdfff65d82d01f6ef4aa7d988fb6e
‘AramK0’ <aramk6827@gmail.com>
2025-11-09 23:09:06 +0300

    header file finish

--- pid.h
+++ pid.h
@@ -12,13 +12,33 @@ typedef struct {
     /* Derivative low-pass filter time constant*/
     float tau;
 
+    /* Output limits*/
+    float limitMin;
+    float limitMax;
+
+    /* Integrator limits*/
+    float limitMinInt;
+    float limitMaxInt;
+    
+    /*sample time in seconds*/
+    float T;
+
+    /* Controller "memory"*/
+    float integrator;
+    float prev_error; // required for integrator
+    float differentiator;
+    float prev_measurement; // required for differentiator 
+
+    /* controller output*/
+    float out;
     
 
 
+} pid_controller;
 
 
+// we pass it a pointer to the structure 
+void pid_controller_init(pid_controller *pid);
+float pid_controller_update(pid_controller *pid , float setpoint, float measurement);
 
-
-
-
-}
+#endif
