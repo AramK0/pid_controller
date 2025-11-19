0fea540cfe5f237dd77460fba98191709228a1a7
Aram <aramk6827@gmail.com>
2025-11-19 11:03:49 +0300

    Refactor PID controller parameters and file output

--- main.c
+++ main.c
@@ -1,52 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include "pid.h"
 
+#include "pid.h"
 
 int main(){
 
+
     pid_controller pid;
 
-    pid.kp = 3.0f;
-    pid.ki = 0.8f;
-    pid.kd = 1.2f;
+    pid.kp = 5.0f;
+    pid.ki = 1.5f;
+    pid.kd = 2.0f;
     pid.tau = 0.02f;
-    pid.limitMin = -10.0f;
-    pid.limitMax = 10.0f;
-    pid.limitMinInt = -5.0f;
-    pid.limitMaxInt = 5.0f;
-    pid.T = 0.01f; // 10 ms loop
+    pid.limit_min = -10.0f;
+    pid.limit_max = 10.0f;
+    pid.limit_min_int = -5.0f;
+    pid.limit_max_int = 5.0f;
+    pid.time = 0.01f; // 0.01 seconds per loop;
 
-    pid_controller_init(&pid);
-    
-    float setpoint = 0.0f; /* Target pitch (degree) of the drone we want it to be sat 0 deg for flat*/
-    float pitch = 10.0f; // starts tilted 10 degress forward
-    float pitch_rate = 0.0f; // angular velocity
+
+
+    pid_init(&pid);
+
+    float setpoint = 5.0f; // Target pitcj (dgree ) of the drone
+    float pitch = 0.0f;
+    float pitch_rate = 0.0f;
     float torque;
-    float inertia = 1.0f; // describes how hard it is for the drone to tilt
+    float inertia = 1.0f;
     float damping = 0.3f;
 
+    FILE *file = fopen("drone_pid.csv", "w");
+    fprintf(file, "Time : Setpoint : Output\n");
 
-    FILE *fl = fopen("drone_pid.csv", "w");
-    fprintf(fl, "time,setpoint,output\n");
+    for(int i = 0; i < 20000; i++){
+        torque = pid_update(&pid, setpoint, pitch);
 
-    for(int i = 0; i < 100; i++){
-        torque = pid_controller_update(&pid, setpoint, pitch);
+        float acceleration = (torque / inertia) - damping * pitch_rate;
 
 
-        // angular acceleration = (torque / inertia) - damping * rate
-        float accel = (torque / inertia) - damping * pitch_rate;
+        pitch_rate = pitch_rate + acceleration * pid.time;
+        pitch = pitch + pitch_rate * pid.time;
 
-        pitch_rate = pitch_rate + accel * pid.T;
-        pitch = pitch + pitch_rate * pid.T;
-
-        fprintf(fl, "%.3f,%.3f, %.3f, %.3f\n", i*pid.T, setpoint, pitch, torque);
+        fprintf(file, "%.3f : %.3f : %.3f\n", i*pid.time, setpoint, pitch);
 
     }
+    fclose(file);
 
-    fclose(fl);
-    printf("drone pitch simulation copied to drone_pid.csv\n");
 
 
 
