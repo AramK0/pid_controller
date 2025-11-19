c662d84a206aaa141a5b622e4864b67bb9bf05d9
Aram <aramk6827@gmail.com>
2025-11-19 11:03:20 +0300

    Update pid.c

--- pid.c
+++ pid.c
@@ -1,73 +1,57 @@
 #include "pid.h"
 
 
-void pid_controller_init(pid_controller *pid){
 
 
 
-    /* clear controller variables*/
-    pid->integrator = 0.0f;
-    pid->prev_error = 0.0f;
+void pid_init(pid_controller *pid){
+    pid->integrator = 0;
+    pid->prev_error = 0;
+    pid->differentiator = 0;
+    pid->prev_measurement = 0;
 
-    pid->differentiator = 0.0f;
-    pid->prev_measurement = 0.0f;
-
-
-    pid->out = 0.0f;
-
+    pid->output = 0;
 }
 
 
-float pid_controller_update(pid_controller *pid, float setpoint, float measurement){
-
-
-
-    /* Error signal*/
-
+float pid_update(pid_controller * pid, float setpoint, float measurement){
 
     float error = setpoint - measurement;
 
     float proportional = pid->kp * error;
 
-    // integrator
-    pid->integrator = pid->integrator * 0.5f * pid->ki * pid->T * (error + pid->prev_error);
 
-    /* anti-windup via integrator clamping*/
+    // integrator formula: delat Ik = Ki * T * (ek + ek-1) /2
+    pid->integrator += pid->ki * 0.5f * pid->time * (error + pid->prev_error);
 
-    if(pid->integrator > pid->limitMaxInt){
-        pid->integrator = pid->limitMaxInt;
+
+    if(pid->integrator > pid->limit_max_int){
+        pid->integrator = pid->limit_max_int;
     }
-    else if(pid->integrator < pid->limitMinInt){
-        pid->integrator = pid->limitMinInt;
+    else if(pid->integrator < pid->limit_min_int){
+        pid->integrator = pid->limit_min_int;
     }
 
-
-    pid->differentiator = -(2.0f * pid->kd * (measurement - pid->prev_measurement) + (2.0f * pid->tau - pid->T) * pid->differentiator)
-                            / (2.0f * pid->tau + pid->T);
-
-
-        /* compute output and apply limits*/
-
-        pid->out = proportional + pid->integrator + pid->differentiator;
-
-        if(pid->out > pid->limitMax){
-            pid->out = pid->limitMax;
-        }
-        else if(pid->out < pid->limitMin){
-            pid->out = pid->limitMin;
-        }
+    pid->differentiator  = -(2.0f * pid->kd * (measurement - pid->prev_measurement))
+                        +   (2.0f * pid->tau - pid->time) * pid->differentiator
+                        / (2.0f * pid->tau + pid->time);
 
 
-        /* store erro and measurement for later use*/
+    pid->output = proportional + pid->integrator + pid->differentiator;
 
-        pid->prev_error = error;
-        pid->prev_measurement = measurement;
+    if(pid->output > pid->limit_max){
+        pid->output = pid->limit_max;
+    }
+    else if(pid->output < pid->limit_min){
+        pid->output = pid->limit_min;
+    }
 
-        // return controller output 
-        return pid->out;
-
-
-
-
+    pid->prev_error = error;
+    pid->prev_measurement = measurement;
+    
+    return pid->output;
 
 }
+
+
+
